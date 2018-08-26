34018fe7170be0da4985dc096c9dded50e1ea56e
Connor Rooney <connor.rooney.15@cnu.eu>
2018-08-26 10:12:37 -0400

    This is the first translation app attempt

---
+++ Python Code/Translator.py
@@ -0,0 +1,11 @@
+from string import whitespace, punctuation
+user_input = input("What do you want to be translated? ")
+input_line = user_input.lower().split()
+dictionary_key = {"want": , "translated:
+
+for element in input_line:
+    if element == "want":
+        #remove want and insert something else
+    if element == "translated":
+        #remove translated and insert something else
+
